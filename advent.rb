14c7bd3a452726fe08298a109deca18474942a16
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2020-12-01 21:28:43 +0100

    tests seem to pass

--- advent.rb
+++ advent.rb
@@ -1,7 +1,16 @@
 module Advent
   class Report
-    def foo
+    def initialize(input)
+      @input = input
+    end
 
+    def calculate
+      @input.each do |number|
+        diff = 2020 - number
+        if @input.include?(diff)
+          return number * diff
+        end
+      end
     end
   end
 end
--- advent_test.rb
+++ advent_test.rb
@@ -5,8 +5,17 @@ require 'mutant/minitest/coverage'
 class AdventTest < Minitest::Test
 
   cover "Advent"
-  def test_fails
-    Advent::Report.new
-    assert true
+  def test_happy
+    input =
+      [
+        1721,
+        979,
+        366,
+        299,
+        675,
+        1456
+      ]
+    report = Advent::Report.new(input)
+    assert_equal(514579, report.calculate)
   end
 end
