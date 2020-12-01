6238e4a0802de96dd64fbe320772a9acd6bc7e37
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2020-12-01 21:52:07 +0100

    2nd part WIP

--- advent.rb
+++ advent.rb
@@ -4,13 +4,24 @@ module Advent
       @input = input
     end
 
-    def calculate
-      @input.each do |number|
-        diff = 2020 - number
-        if @input.include?(diff)
+    def calculate_2_numbers(target, numbers = @input)
+      numbers.each do |number|
+        diff = target - number
+        if numbers.include?(diff)
           return number * diff
         end
       end
     end
+
+    def calculate_3_numbers(numbers = @input)
+      numbers.each do |number|
+        diff = 2020 - number
+        calculate_2_numbers(diff, numbers)
+      end
+
+    end
+
   end
 end
+
+#puts Advent::Report.new(File.readlines('input_1.txt').map(&:to_i)).calculate_2_numbers
--- advent_test.rb
+++ advent_test.rb
@@ -5,7 +5,7 @@ require 'mutant/minitest/coverage'
 class AdventTest < Minitest::Test
 
   cover "Advent"
-  def test_happy
+  def test_2_numbers
     input =
       [
         1721,
@@ -16,6 +16,17 @@ class AdventTest < Minitest::Test
         1456
       ]
     report = Advent::Report.new(input)
-    assert_equal(514579, report.calculate)
+    assert_equal(514579, report.calculate_2_numbers(2020))
+  end
+
+  def test_3_numbers
+    input =
+      [
+        979,
+        366,
+        675
+      ]
+    report = Advent::Report.new(input)
+    assert_equal(241861950, report.calculate_3_numbers)
   end
 end
