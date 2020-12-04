d688a0e7d000bdff6eb815188b51493e8bbd426c
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2020-12-04 21:43:04 +0100

    day 2, part 2 done

--- passwords.rb
+++ passwords.rb
@@ -3,7 +3,7 @@ module Advent
     class InvalidPassword < StandardError; end
 
     class Password
-      def self.with_policy(password_with_policy)
+      def self.with_ranged_policy(password_with_policy)
         policy_content = password_with_policy.split(": ")[0]
         password       = password_with_policy.split(": ")[1]
 
@@ -13,7 +13,21 @@ module Advent
         how_many_start = how_many.split('-')[0].to_i
         how_many_end   = how_many.split('-')[1].to_i
 
-        policy = Policy.new(how_many_start..how_many_end, what)
+        policy = RangedCharacterPolicy.new(how_many_start..how_many_end, what)
+        Password.new(password, policy)
+      end
+
+      def self.with_positioned_policy(password_with_policy)
+        policy_content = password_with_policy.split(": ")[0]
+        password       = password_with_policy.split(": ")[1]
+
+        positions = policy_content.split(' ')[0]
+        what     = policy_content.split(' ')[1]
+
+        first_position    = positions.split('-')[0].to_i
+        second_position   = positions.split('-')[1].to_i
+
+        policy = PositionedPolicy.new(first_position, second_position, what)
         Password.new(password, policy)
       end
 
@@ -22,7 +36,7 @@ module Advent
       end
     end
 
-    class Policy
+    class RangedCharacterPolicy
       def initialize(how_many, what)
         @how_many = how_many
         @what     = what
@@ -30,6 +44,19 @@ module Advent
 
       def matches?(password_content)
         @how_many === password_content.count(@what)
+      end
+    end
+
+    class PositionedPolicy
+      def initialize(first_position, second_position, character)
+        @first_postion   = first_position
+        @second_position = second_position
+        @character       = character
+      end
+
+      def matches?(password_content)
+        (password_content[@first_postion-1] == @character) && (password_content[@second_position-1] != @character) ||
+        (password_content[@first_postion-1] != @character) && (password_content[@second_position-1] == @character)
       end
     end
 
@@ -41,7 +68,7 @@ lines = File.readlines("./input_2.txt")
 count = 0
 lines.count do |line|
   begin
-    Advent::Passwords::Password.with_policy(line)
+    Advent::Passwords::Password.with_positioned_policy(line)
     count += 1
   rescue
     next
--- passwords_test.rb
+++ passwords_test.rb
@@ -4,17 +4,31 @@ require 'mutant/minitest/coverage'
 
 module Advent
   module Passwords
-    class PasswordsTest < Minitest::Test
+    class RangedPasswordsTest < Minitest::Test
 
       cover "Advent::Passwords"
 
       def test_valid
-        assert Password.with_policy("1-3 a: abcde")
+        assert Password.with_ranged_policy("1-3 a: abcde")
       end
 
       def test_invalid
         assert_raises InvalidPassword do
-          Password.with_policy("1-3 b: cdefg")
+          Password.with_ranged_policy("1-3 b: cdefg")
+        end
+      end
+    end
+
+    class PositionedPasswordsTest < Minitest::Test
+      cover "Advent::Passwords"
+
+      def test_valid
+        assert Password.with_positioned_policy("1-3 a: abcde")
+      end
+
+      def test_invalide
+        assert_raises InvalidPassword do
+          Password.with_positioned_policy("1-3 b: cdefg")
         end
       end
     end
