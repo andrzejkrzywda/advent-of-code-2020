ad68dbc65bd935716ab36ed9748482afba0bfe0b
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2020-12-05 17:25:48 +0100

    day 3, initial

---
+++ trajectory.rb
@@ -0,0 +1,6 @@
+module Advent
+  module Trajectory
+
+
+  end
+end
---
+++ trajectory_test.rb
@@ -0,0 +1,16 @@
+require './trajectory'
+require 'minitest/autorun'
+require 'mutant/minitest/coverage'
+
+module Advent
+  module Trajectory
+    class TrajectoryTest < Minitest::Test
+
+      cover "Advent::Trajectory"
+
+      def test_valid
+      end
+    end
+
+  end
+end
