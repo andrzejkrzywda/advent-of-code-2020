0c7dd01b57c349ca4618fa1c5a0e1c933e6ef5d8
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2020-12-07 19:51:43 +0100

    day 4 part 1

--- passports.rb
+++ passports.rb
@@ -1,6 +1,54 @@
 module Advent
   module Passports
+    class Verifier
+      def initialize(input)
+        @passports = Builder.new(input).build_passports
+      end
 
+      def correct_count
+        @passports.count {|passport| passport.valid?}
+      end
+    end
+
+    class Builder
+      def initialize(input_string)
+        @input_string = input_string
+      end
+
+      def build_passports
+        @input_string.split("\n\n").map do |passport_fields|
+          Passport.new(passport_fields.split(Regexp.union(delimeteres)))
+        end
+      end
+
+      private
+
+      def delimeteres
+        ["\n", " "]
+      end
+    end
+
+    class Passport
+      def initialize(fields)
+        @fields = fields
+      end
+
+      def valid?
+        all_fields? or all_but_cid?
+      end
+
+      private
+
+      def all_but_cid?
+        @fields.length == 7 && ! @fields.detect{|field| field.split(":")[0] == "cid"}
+      end
+
+      def all_fields?
+        @fields.length == 8
+      end
+    end
   end
 end
+
+puts Advent::Passports::Verifier.new(File.read("./input_4.txt")).correct_count
 
--- passports_test.rb
+++ passports_test.rb
@@ -9,7 +9,23 @@ module Advent
       cover "Advent::Passports"
 
       def test_valid
-        assert true
+        passports_input =
+<<PASSPORTS
+ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
+byr:1937 iyr:2017 cid:147 hgt:183cm
+
+iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884
+hcl:#cfa07d byr:1929
+
+hcl:#ae17e1 iyr:2013
+eyr:2024
+ecl:brn pid:760753108 byr:1931
+hgt:179cm
+
+hcl:#cfa07d eyr:2025 pid:166559648
+iyr:2011 ecl:brn hgt:59in
+PASSPORTS
+        assert_equal(2, Verifier.new(passports_input).correct_count)
       end
 
     end
