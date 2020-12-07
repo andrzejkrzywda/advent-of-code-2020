d5484145ba0830deecaf4008548b8f33e2050796
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2020-12-07 20:57:40 +0100

    day 4 part 2

--- passports.rb
+++ passports.rb
@@ -34,7 +34,7 @@ module Advent
       end
 
       def valid?
-        all_fields? or all_but_cid?
+        (all_fields? or all_but_cid?) and all_existing_fields_valid?
       end
 
       private
@@ -45,6 +45,115 @@ module Advent
 
       def all_fields?
         @fields.length == 8
+      end
+
+      def all_existing_fields_valid?
+        valid_byr? and valid_iyr? and valid_eyr? and valid_hgt? and
+          valid_hcl? and valid_ecl? and valid_pid?
+      end
+
+      def valid_byr?
+        byr.length == 4 and byr.to_i >= 1920 and byr.to_i <= 2002
+      end
+
+      def valid_iyr?
+        iyr.length == 4 and iyr.to_i >= 2010 and iyr.to_i <= 2020
+      end
+
+      def valid_eyr?
+        eyr.length == 4 and eyr.to_i >= 2020 and eyr.to_i <= 2030
+      end
+
+      def valid_hgt?
+        HeightBuilder.build(hgt).valid?
+      end
+
+      def valid_hcl?
+        hcl.length == 7 and
+          hcl[0] == "#" and
+          (
+            hcl.partition("#").last.chars.all?{|char| 0<= char.to_i and char.to_i <= 9} or
+            hcl.partition("#").last.chars.all?{|char| "a" <= char and char <= "f"}
+          )
+      end
+
+      def valid_ecl?
+        %w[amb blu brn gry grn hzl oth].include?(ecl)
+      end
+
+      def valid_pid?
+        pid.length == 9 and pid.chars.all?{|char| 0<= char.to_i and char.to_i <= 9}
+      end
+
+      def byr
+        find_field("byr")
+      end
+
+      def iyr
+        find_field("iyr")
+      end
+
+      def eyr
+        find_field("eyr")
+      end
+
+      def hgt
+        find_field("hgt")
+      end
+
+      def hcl
+        find_field("hcl")
+      end
+
+      def ecl
+        find_field("ecl")
+      end
+
+      def pid
+        find_field("pid")
+      end
+
+      def find_field(name)
+        @fields.detect{|field| field.split(":")[0] == name}.split(":")[1]
+      end
+
+    end
+
+    class HeightBuilder
+      def self.build(content)
+        if content.end_with?("cm")
+          CmHeight.new(content.partition("cm").first)
+        elsif content.end_with?("in")
+          InHeight.new(content.partition("in").first)
+        else
+          InvalidHeight.new
+        end
+      end
+    end
+
+    class CmHeight
+      def initialize(content)
+        @content = content
+      end
+
+      def valid?
+        150 <= @content.to_i and @content.to_i <= 193
+      end
+    end
+
+    class InHeight
+      def initialize(content)
+        @content = content
+      end
+
+      def valid?
+        59 <= @content.to_i and @content.to_i <= 76
+      end
+    end
+
+    class InvalidHeight
+      def valid?
+        false
       end
     end
   end
