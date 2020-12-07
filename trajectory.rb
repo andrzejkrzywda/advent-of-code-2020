0d4c6ae4597f9d0c1e9420c79c8def5ac5df9704
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2020-12-07 11:17:46 +0100

    day 3, part 2

--- trajectory.rb
+++ trajectory.rb
@@ -2,11 +2,12 @@ module Advent
   module Trajectory
 
     class TreesEncounter
-      def initialize(map_template)
+      def initialize(map_template, slope)
         puts "initialize for: [#{map_template.length}, #{map_template[0].length}]"
         puts map_template[0].inspect
         @map = build_map(map_template)
         @location = [0, 0]
+        @slope = slope
       end
 
       def trees_count
@@ -19,8 +20,8 @@ module Advent
       end
 
       def jump
-        @location[0] = @location[0] + 1
-        @location[1] = @location[1] + 3
+        @location[0] = @location[0] + @slope.right
+        @location[1] = @location[1] + @slope.down
       end
 
       private
@@ -44,10 +45,34 @@ module Advent
       def depth
         @map_template.length
       end
+    end
 
+    class Slope
+      attr_reader :right, :down
 
+      def initialize(right, down)
+        @right = right
+        @down  = down
+      end
     end
   end
 end
 
-puts Advent::Trajectory::TreesEncounter.new(File.readlines("./input_3.txt", chomp: true)).trees_count
+map_template = File.readlines("./input_3.txt", chomp: true)
+slopes =
+  [
+    Advent::Trajectory::Slope.new(1, 1),
+    Advent::Trajectory::Slope.new(1, 3),
+    Advent::Trajectory::Slope.new(1, 5),
+    Advent::Trajectory::Slope.new(1, 7),
+    Advent::Trajectory::Slope.new(2, 1)
+  ]
+
+trees_counters = []
+slopes.each do |slope|
+  trees_encounter  = Advent::Trajectory::TreesEncounter.new(map_template, slope)
+  trees_counters << trees_encounter.trees_count
+end
+
+puts trees_counters.inject(&:*)
+
