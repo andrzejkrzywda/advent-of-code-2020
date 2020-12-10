9ee45c4191ac66ff8a39831ca728bf110bd9ef64
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2020-12-10 14:58:47 +0100

    day 5, part 1

--- boarding.rb
+++ boarding.rb
@@ -1,5 +1,26 @@
 module Advent
   module Boarding
+    class RowAndSeatFinder
+      def initialize(code)
+        @code = code
+      end
 
+      def row
+        @code[0...7].gsub("F", "0").gsub("B", "1").to_i(2)
+      end
+
+      def seat
+        @code[7..9].gsub("R", "1").gsub("L", "0").to_i(2)
+      end
+
+      def seat_id
+        row * 8 + seat
+      end
+    end
   end
 end
+
+codes = File.readlines("./input_5.txt")
+finders = codes.map{|code| Advent::Boarding::RowAndSeatFinder.new(code)}
+seat_ids = finders.map{|finder| finder.seat_id}
+puts seat_ids.max
--- boarding_test.rb
+++ boarding_test.rb
@@ -3,14 +3,25 @@ require 'minitest/autorun'
 require 'mutant/minitest/coverage'
 
 module Advent
-  module Passports
-    class PassportsTest < Minitest::Test
+  module Boarding
+    class BoardingTest < Minitest::Test
 
       cover "Advent::Boarding"
 
       def test_works
-        assert true
+        assert_equal(44, RowAndSeatFinder.new("FBFBBFFRLR").row)
+        assert_equal(5, RowAndSeatFinder.new("FBFBBFFRLR").seat)
+        assert_equal(357, RowAndSeatFinder.new("FBFBBFFRLR").seat_id)
+
+        assert_equal(14, RowAndSeatFinder.new("FFFBBBFRRR").row)
+        assert_equal(7, RowAndSeatFinder.new("FFFBBBFRRR").seat)
+        assert_equal(119, RowAndSeatFinder.new("FFFBBBFRRR").seat_id)
+
+        assert_equal(102, RowAndSeatFinder.new("BBFFBBFRLL").row)
+        assert_equal(4, RowAndSeatFinder.new("BBFFBBFRLL").seat)
+        assert_equal(820, RowAndSeatFinder.new("BBFFBBFRLL").seat_id)
       end
+
     end
   end
 end
